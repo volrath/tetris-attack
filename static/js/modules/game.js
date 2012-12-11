85e362d24093058fb6897c5f45b02509663f3c30
Daniel Barreto <dbarreto@talpor.com>
2012-12-10 20:24:50 -0430

    using our new events module in game.js, right now it only initializes every event listener and I added a console.log into tick to show the catched events, it's commented out though.

--- static/js/modules/game.js
+++ static/js/modules/game.js
@@ -1,26 +1,47 @@
-define(['modules/helpers/loader', 'easel'], function(loader) {
+define(['modules/helpers/loader', 'modules/helpers/events', 'easel'], function(loader, events) {
     var stages = {
         blue: new createjs.Stage(document.getElementById('blue-player')),
         red:  new createjs.Stage(document.getElementById('red-player'))
     };
+    var boards;
 
+    /*
+     * Init: initializes event listening, preloads all assets and starts the
+     * game. Then adds `tick` to listen to createjs Ticker's events.
+     */
     var init = function (preload, callback) {
         createjs.Ticker.useRAF = true;
         createjs.Ticker.setFPS(36);
 
-        //events.init();
+        events.init();
 
         loader.preload(
             preload,
             function () {
                 if (callback) callback();
+                start();
                 createjs.Ticker.addListener(tick);
             }
         );
     };
 
+    /*
+     * Start: runs once before the game starts ticking, this is useful to
+     * initialize game's objects.
+     */
+    var start = function () {
+
+    };
+
+    /*
+     * Tick: ticking function, will be called once 1/fps milliseconds.
+     */
     var tick = function (msDuration) {
         //console.info(msDuration);
+
+        // var evs = events.get();
+        // if (evs.length)
+        //     console.log(evs);
 
         for (var key in stages)
             stages[key].update();
