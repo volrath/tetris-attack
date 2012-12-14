b7b3870b408c8405e1ef29f0794db6a1009522da
Daniel Barreto <dbarreto@talpor.com>
2012-12-13 19:46:43 -0430

    adding a new Block class that inheritance from createjs.Bitmap.

---
+++ static/js/modules/block.js
@@ -0,0 +1,19 @@
+define(['modules/helpers/loader', 'modules/globals', 'easel'], function(loader, globals) {
+    var Block = function (x, y, i, j, color) {
+        this.x = x;
+        this.y = y;
+        this.i = i;
+        this.j = j;
+        this.color = color;
+
+        this.initialize(loader.get(globals.assets.blocks[color]));
+    };
+
+    Block.prototype = new createjs.Bitmap();
+
+    Block.prototype.onTick = function (msDuration) {
+        //this.y -= 0.2;
+    };
+
+    return Block;
+});
