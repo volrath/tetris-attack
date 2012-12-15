bb9ad68b748edd3fbec5da561164c88132516d43
Daniel Barreto <dbarreto@talpor.com>
2012-12-15 16:05:55 -0430

    adding a `matched` attr and `isMatchable` method to `Block`. also override `Block`'s draw method to show some debug information when the block is matched.

--- static/js/modules/block.js
+++ static/js/modules/block.js
@@ -5,14 +5,30 @@ define(['modules/helpers/loader', 'modules/globals', 'easel'], function(loader, 
         this.i = i;
         this.j = j;
         this.color = color;
+        this.matched = false;
 
         this.initialize(loader.get(globals.assets.blocks[color]));
     };
-
     Block.prototype = new createjs.Bitmap();
 
+    Block.prototype.p_draw = Block.prototype.draw;
+    Block.prototype.draw = function (ctx, ignoreCache) {
+        var p_draw = this.p_draw(ctx, ignoreCache);
+        if (p_draw && this.matched) {
+            ctx.beginPath();
+            ctx.moveTo(0, 0);
+            ctx.lineTo(50, 50);
+            ctx.stroke();
+            ctx.fillText('(' + this.i + ', ' + this.j + ')', 10, 10);
+        }
+        return p_draw;
+    };
+
+    Block.prototype.isMatchable = function () {
+        return true;
+    };
+
     Block.prototype.onTick = function (msDuration) {
-        this.y -= globals.difficulty['easy'].speed;
         this.alpha = this.y <= 500 ? 1 : 0.3;
     };
 
